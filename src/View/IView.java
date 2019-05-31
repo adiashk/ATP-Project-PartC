0648035bf88fb2c94b51b55eb6efd138ea5c3973
adiashk <ashkenazi94>
2019-05-31 17:44:50 +0300

    build menu bar

--- src/View/IView.java
+++ src/View/IView.java
@@ -1,4 +1,12 @@
 package View;
 
 public interface IView {
+    public void newGame();
+    public void saveGame();
+    public void loadGame();
+    public void propertiesGame();
+    public void exitGame();
+    public void helpGame();
+    public void aboutGame();
+
 }
--- src/View/MyViewController.java
+++ src/View/MyViewController.java
@@ -1,5 +1,38 @@
 package View;
 
 public class MyViewController implements IView{
+    @Override
+    public void newGame() {
 
+    }
+
+    @Override
+    public void saveGame() {
+
+    }
+
+    @Override
+    public void loadGame() {
+
+    }
+
+    @Override
+    public void propertiesGame() {
+
+    }
+
+    @Override
+    public void exitGame() {
+
+    }
+
+    @Override
+    public void helpGame() {
+
+    }
+
+    @Override
+    public void aboutGame() {
+
+    }
 }
